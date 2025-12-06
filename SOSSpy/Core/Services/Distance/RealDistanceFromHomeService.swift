7980bd09a2e8bc71090c35459d254e0cea16c88e
Alex Harris <alexharris0079@gmail.com>
2025-12-07 05:51:32 +1000

    [SOSSpy] feat: (Core/Services) read simulator location in real distance service

--- SOSSpy/Core/Services/Distance/RealDistanceFromHomeService.swift
+++ SOSSpy/Core/Services/Distance/RealDistanceFromHomeService.swift
@@ -2,6 +2,7 @@
 // Real implementation of DistanceFromHomeService
 
 import Foundation
+import CoreLocation
 
 /// Real distance from home service implementation
 /// Calculates distance using haversine formula with frozen Bundaberg coordinates
@@ -13,10 +14,19 @@ struct RealDistanceFromHomeService: DistanceFromHomeService {
     func distanceFromHome(
         current: (Double, Double)
     ) async throws -> Double {
+        // Try to get actual device/simulator location
+        let actualLocation: (Double, Double)
+        if let location = CLLocationManager().location {
+            actualLocation = (location.coordinate.latitude, location.coordinate.longitude)
+        } else {
+            // Fall back to input tuple if location unavailable
+            actualLocation = current
+        }
+
         let lat1 = homeCoordinates.0 * .pi / 180.0
         let lon1 = homeCoordinates.1 * .pi / 180.0
-        let lat2 = current.0 * .pi / 180.0
-        let lon2 = current.1 * .pi / 180.0
+        let lat2 = actualLocation.0 * .pi / 180.0
+        let lon2 = actualLocation.1 * .pi / 180.0
 
         let dLat = lat2 - lat1
         let dLon = lon2 - lon1
